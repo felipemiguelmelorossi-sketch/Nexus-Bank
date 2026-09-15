5cff979e9a9695ca963bc91201cac3d097af7b2d
felipemiguelmelorossi-sketch <felipemiguelmelorossi@gmail.com>
2026-09-15 17:23:28 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,11 @@
 const SUPABASE_URL = "https://dnojapjtsgxsgmxxjhrp.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "SUA_CHAVE_PUBLICA_AQUI";
 
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_zZy8CirGsWqjzYIv2GIanA_BQaASny8";
+console.log("SUPABASE_URL:", SUPABASE_URL);
 
 window.supabaseClient = window.supabase.createClient(
 SUPABASE_URL,
 SUPABASE_PUBLISHABLE_KEY
 );
+
+console.log("Supabase carregado:", !!window.supabaseClient);
