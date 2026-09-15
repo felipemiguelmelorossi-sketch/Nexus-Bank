da4ec8cf4edfa77729b90d49906fe7f8069597b5
felipemiguelmelorossi-sketch <felipemiguelmelorossi@gmail.com>
2026-09-15 16:13:24 -0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://dnojapjtsgxsgmxxjhrp.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_zZy8CirGsWqjzYIv2GIanA_BQaASny8";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
